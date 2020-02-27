494cc0e39fce8c9317cc5be896b270aa75bfa079
Jessica Nguyen <jdn039@ucsd.edu>
2020-02-27 12:44:00 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = 'caES7vFZiuvdY3iIXfNLszBfHW5f4Py5';
+
+  // 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.878992, -117.235698],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12,
+    zoomControl: false
+  });
+
+  L.marker([32.878992, -117.235698]).addTo(map);
 }
